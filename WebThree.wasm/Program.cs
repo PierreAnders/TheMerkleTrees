0760d27ec3d60b47599fe350a18c225368db799e
PierreAnders <pierre.untas@gmail.com>
2024-06-08 22:28:17 +0200

    first commit

---
+++ WebThree.wasm/Program.cs
@@ -0,0 +1,20 @@
+using Blazored.SessionStorage;
+using MetaMask.Blazor;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using WebThree.wasm;
+using WebThree.wasm.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddMetaMaskBlazor();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddBlazoredSessionStorage();
+builder.Services.AddScoped<AuthenticationStateProvider, EthereumAuthenticationStateProvider>();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
