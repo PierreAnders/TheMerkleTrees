692b3aecca18f8f9abee4860627c8e367741caa1
PierreAnders <pierre.untas@gmail.com>
2024-06-22 23:47:51 +0200

    send file to api to send then to ipfs

---
+++ WebThree.client/wwwroot/downloadFile.js
@@ -0,0 +1,8 @@
+﻿function downloadFileFromUrl(fileName, fileUrl) {
+    var link = document.createElement('a');
+    link.download = fileName;
+    link.href = fileUrl;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
