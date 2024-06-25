2bf9ec62ef467cc3f8d286e73394dce1b4ba13dd
PierreAnders <pierre.untas@gmail.com>
2024-06-26 00:31:10 +0200

    reload ok

---
+++ WebThree.client/Models/File.cs
@@ -0,0 +1,11 @@
+﻿namespace WebThree.client.Models;
+
+public class File
+{
+    public string Id { get; set; } = null!;
+    public string Hash { get; set; } = null!;
+    public string Name { get; set; } = null!;
+    public string Category { get; set; } = null!;
+    public bool IsPublic { get; set; }
+    public string Owner { get; set; } = null!;
+}
